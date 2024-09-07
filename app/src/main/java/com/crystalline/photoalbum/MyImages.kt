fb883286717a7a5f3294570258855117cd816f06
Chien-Shan Peng <crystalline@crystalline.work>
2024-09-07 13:11:40 -0700

    init the project

---
+++ app/src/main/java/com/crystalline/photoalbum/MyImages.kt
@@ -0,0 +1,14 @@
+package com.crystalline.photoalbum
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "my_images")
+class MyImages (
+    val imageTitle : String,
+    val imageDescription : String,
+    var imageAsString : String
+) {
+    @PrimaryKey(autoGenerate = true)
+    val imageId = 0
+}
