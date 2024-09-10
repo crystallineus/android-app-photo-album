1b58a862c7fb0c836cc6b0668297fd92620437b6
Chien-Shan Peng <crystalline@crystalline.work>
2024-09-09 23:37:59 -0700

    feat: create repository

---
+++ app/src/main/java/com/crystalline/photoalbum/repository/MyImagesRepository.kt
@@ -0,0 +1,34 @@
+package com.crystalline.photoalbum.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.crystalline.photoalbum.model.MyImages
+import com.crystalline.photoalbum.room.MyImagesDao
+import com.crystalline.photoalbum.room.MyImagesDatabase
+
+class MyImagesRepository(application: Application) {
+    var myImagesDao : MyImagesDao
+    var imagesList : LiveData<List<MyImages>>
+
+    init {
+        val database = MyImagesDatabase.getDatabaseInstance(application)
+        myImagesDao = database.myImagesDao()
+        imagesList = myImagesDao.getAll()
+    }
+
+    suspend fun insert(myImages: MyImages) {
+        myImagesDao.insert(myImages)
+    }
+
+    suspend fun update(myImages: MyImages) {
+        myImagesDao.update(myImages)
+    }
+
+    suspend fun delete(myImages: MyImages) {
+        myImagesDao.delete(myImages)
+    }
+
+    fun getAll() : LiveData<List<MyImages>> {
+        return imagesList
+    }
+}
